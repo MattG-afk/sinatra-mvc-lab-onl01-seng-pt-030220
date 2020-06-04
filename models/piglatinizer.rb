bc908672f1db57de5643cba3fd81a5858d5ebd4d
Matt Gordon <bigmattsize21@gmail.com>
2020-06-04 00:43:25 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -12,8 +12,8 @@ def piglatinize(word) #pig latinizes a single word
   end
 end
 
-def to_pig_latin(phrase)
-  word_array = phrase.split(" ")
+def to_pig_latin(word)
+  word_array = word.split(" ")
   @piglatinized_phrase = word_array.collect {|word| piglatinize(word)}.join(" ")
 end
 end 
